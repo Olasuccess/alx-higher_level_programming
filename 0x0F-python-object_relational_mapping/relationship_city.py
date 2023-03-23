13181c0237fbf13df94f4515046492c6d27866ff
olasuccess <walesuccess02@gmail.com>
2023-03-23 18:34:51 +0000

    mapping

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""
-Class definition of a City with inheritage from Base
-"""
+"""This is the City module.
 
+Contains the City class that inherits from Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
 from relationship_state import Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
+    """This class links to the `cities` table of our database.
+
+    Attributes:
+        id (int): id of the city.
+        name (str): name of the city.
+        state_id (int): id of the associated state.
     """
-    City Class
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
