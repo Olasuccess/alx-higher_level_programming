383db2d5284f8454060fc629d67e58141b4c10a7
olasuccess <walesuccess02@gmail.com>
2023-06-19 07:30:13 +0100

    web jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+$(function () {
+  const $movies = $('#list_movies');
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi.co/api/films/?format=json',
+    success: function (data) {
+	    $.each(data.results, function (i, movie) {
+        $movies.append('<li>' + movie.title + '</li>');
+	    });
+    }
+  });
+});
